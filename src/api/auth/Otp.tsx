282dae4e19f551150eb7d714dd07c6a35b0b299f
bhargav.harvani <bhargav.harvani@esparkbizmail.com>
2025-08-07 11:56:21 +0530

    timeout changes

--- src/api/auth/Otp.tsx
+++ src/api/auth/Otp.tsx
@@ -17,7 +17,7 @@ import { toast } from "react-toastify";
 
 const axiosInstance = axios.create({
   baseURL: "https://inventory-backend-hjmi.onrender.com/",
-  timeout: 5000,
+  // timeout: 5000,
   withCredentials: true,
 });
 
